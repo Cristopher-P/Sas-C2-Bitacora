703fe48dbc5e1a6e1be5957e260b38a88ed6f4fd
jjbvx <cristo261105@gmail.com>
2026-02-06 12:29:06 -0600

    fix/ Uso de MYSQL_URL para la conexion de Railway

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,6 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool(process.env.MYSQL_URL);
+console.log('MYSQL_URL:', process.env.MYSQL_URL ? 'OK' : 'NO DEFINIDA');
 
-module.exports = pool;
+module.exports = pool.promise();
